8c8ec3a8fac096cf2eac87a7e22e071befbbc9f3
Yokesh-3104 <74831122+Yokesh-3104@users.noreply.github.com>
2021-04-17 15:13:29 +0530

    Add files via upload

---
+++ ipynb.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import pandas as pd
+df = pd.read_csv(r"C:\Users\PC\Desktop\Heart_dataset.csv")
+df
+
+
+# In[4]:
+
+
+df.shape
+
+
+# In[5]:
+
+
+df.target.value_counts()
+
+
+# In[6]:
+
+
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+import numpy as np
+
+
+# In[7]:
+
+
+df.target.value_counts().plot(kind="bar", color=["salmon", "lightblue"])
+
+
+# In[8]:
+
+
+## Checking for missing values
+df.isna().sum()
+
+
+# In[9]:
+
+
+categorical_val = []
+continous_val = []
+for column in df.columns:
+    print('==============================')
+    print(f"{column} : {df[column].unique()}")
+    if len(df[column].unique()) <= 10:
+        categorical_val.append(column)
+    else:
+        continous_val.append(column)
+
+
+# In[10]:
+
+
+plt.figure(figsize=(15, 15))
+
+for i, column in enumerate(categorical_val, 1):
+    plt.subplot(3, 3, i)
+    df[df["target"] == 0][column].hist(bins=35, color='blue', label='Have Heart Disease = NO', alpha=0.6)
+    df[df["target"] == 1][column].hist(bins=35, color='red', label='Have Heart Disease = YES', alpha=0.6)
+    plt.legend()
+    plt.xlabel(column)
+    
+
+
+# In[11]:
+
+
+plt.figure(figsize=(15, 15))
+
+for i, column in enumerate(continous_val, 1):
+    plt.subplot(3, 2, i)
+    df[df["target"] == 0][column].hist(bins=35, color='blue', label='Have Heart Disease = NO', alpha=0.6)
+    df[df["target"] == 1][column].hist(bins=35, color='red', label='Have Heart Disease = YES', alpha=0.6)
+    plt.legend()
+    plt.xlabel(column)
+    
+
+
+# In[12]:
+
+
+## Age vs Max Heart rate for heart disease
+plt.figure(figsize=(10, 8))
+
+# Scatter with postivie examples
+plt.scatter(df.age[df.target==1],
+            df.thalach[df.target==1],
+            c="salmon")
+
+# Scatter with negative examples
+plt.scatter(df.age[df.target==0],
+            df.thalach[df.target==0],
+            c="lightblue")
+
+# Adding some helpful info
+plt.title("Heart Disease in function of Age and Max Heart Rate")
+plt.xlabel("Age")
+plt.ylabel("Max Heart Rate")
+plt.legend(["Disease", "No Disease"]);
+
+
+# In[13]:
+
+
+## Machine Learning algorithms
+
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+def print_score(clf, X_train, y_train, X_test, y_test, train=True):
+    if train:
+        pred = clf.predict(X_train)
+        clf_report = pd.DataFrame(classification_report(y_train, pred, output_dict=True))
+        print("Train Result:\n================================================")
+        print(f"Accuracy Score: {accuracy_score(y_train, pred) * 100:.2f}%")
+        print("_______________________________________________")
+        print(f"CLASSIFICATION REPORT:\n{clf_report}")
+        print("_______________________________________________")
+        print(f"Confusion Matrix: \n {confusion_matrix(y_train, pred)}\n")
+        
+    elif train==False:
+        pred = clf.predict(X_test)
+        clf_report = pd.DataFrame(classification_report(y_test, pred, output_dict=True))
+        print("Test Result:\n================================================")        
+        print(f"Accuracy Score: {accuracy_score(y_test, pred) * 100:.2f}%")
+        print("_______________________________________________")
+        print(f"CLASSIFICATION REPORT:\n{clf_report}")
+        print("_______________________________________________")
+        print(f"Confusion Matrix: \n {confusion_matrix(y_test, pred)}\n")
+
+
+# In[15]:
+
+
+from sklearn.model_selection import train_test_split
+
+X = df.drop('target', axis=1)
+y = df.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+## Using logistic regression model
+
+from sklearn.linear_model import LogisticRegression
+
+lr_clf = LogisticRegression(solver='liblinear')
+lr_clf.fit(X_train, y_train)
+
+print_score(lr_clf, X_train, y_train, X_test, y_test, train=True)
+print_score(lr_clf, X_train, y_train, X_test, y_test, train=False)
+
+
+# In[16]:
+
+
+test_score = accuracy_score(y_test, lr_clf.predict(X_test)) * 100
+train_score = accuracy_score(y_train, lr_clf.predict(X_train)) * 100
+
+results_df = pd.DataFrame(data=[["Logistic Regression", train_score, test_score]], 
+                          columns=['Model', 'Training Accuracy %', 'Testing Accuracy %'])
+results_df
+
+
+# In[ ]:
+
+
+
+
